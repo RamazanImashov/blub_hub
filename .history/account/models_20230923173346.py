37cae27098479bc46bd4d123b3b09489fa5c0545
kasenov09 <kasenov09>
2023-09-27 11:59:05 +0600

    subscribe

---
+++ .history/account/models_20230923173346.py
@@ -0,0 +1,66 @@
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractUser
+from django.utils.crypto import get_random_string
+
+
+# Create your models here.
+
+class UserManager(BaseUserManager):
+    def _create(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError(
+                'Email not be not'
+            )
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.create_activation_code()
+        user.create_forgot_password_code()
+        user.save()
+        return user
+
+    def create_user(self, email, password, **extra_fields):
+        return self._create(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self._create(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+    login = models.CharField(max_length=12, unique=True)
+    is_active = models.BooleanField(default=False)
+    activation_code = models.CharField(max_length=20, blank=True)
+    forgot_password_code = models.CharField(max_length=20, blank=True)
+
+    objects = UserManager()
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["login"]
+
+    def __str__(self):
+        return f'{self.id} {self.email}'
+
+    def create_activation_code(self):
+        code = get_random_string(length=10, allowed_chars='0123456789')
+        self.activation_code = code
+
+    def create_forgot_password_code(self):
+        code = get_random_string(length=10, allowed_chars='0123456789')
+        self.forgot_password_code = code
+
+from django.db import models
+from django.contrib.auth.models шьзщке get_user_model
+
+# Create your models here.
+
+User = get_user_model()
+class Subscription(models.Model):
+    subscriber = models.ForeignKey(User, on_delete=models.CASCADE, related_name='subscriptions')
+    target_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='subscribers')
+
+    class Meta:
+        unique_together = ('subscriber', 'target_user')
---
+++ .history/apps/review/models_20230923155831.py
@@ -0,0 +1,62 @@
+from django.db import models
+from apps.video.models import Video
+from django.contrib.auth import get_user_model
+
+# Create your models here.
+
+User = get_user_model()
+
+
+class Comment(models.Model):
+    body = models.TextField(verbose_name='Description')
+    video = models.ForeignKey(Video, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    files = models.FileField(upload_to='comment_files/', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.author}, {self.body}'
+
+
+class Rating(models.Model):
+    rating = models.PositiveSmallIntegerField()
+    video = models.ForeignKey(Video, on_delete=models.CASCADE, related_name='ratings')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ratings')
+
+    def __str__(self):
+        return f'{self.author} {self.rating}'
+
+
+class Like(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
+    video = models.ForeignKey(Video, on_delete=models.CASCADE, related_name='likes')
+
+    def __str__(self):
+        return f'{self.author}{self.video}'
+
+
+class Dislike(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='dislikes')
+    video = models.ForeignKey(Video, on_delete=models.CASCADE, related_name='dislikes')
+
+    def __str__(self):
+        return f'{self.author}{self.video}'
+
+
+class WatchLater(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='watch_later')
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('-pk',)
+        constraints = [
+            models.UniqueConstraint(fields=['author', 'video'], name='unique_author_video'),
+        ]
+        indexes = [
+            models.Index(fields=['author', 'video'], name='index_author_video'),
+        ]
+    class Subscription(models.Model):
+    subscriber = models.ForeignKey(User, related_name='subscriptions', on_delete=models.CASCADE)
+    target_user = models.ForeignKey(User, related_name='subscribers', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
